836f98a25bcaf68839d22b6fd9dcababab2ef1a4
chrisfeatley <chris.featley@yodel.co.uk>
2022-02-20 22:12:09 +0000

    Testing of filterByCategory

--- src/utils/dataFunctions.test.js
+++ src/utils/dataFunctions.test.js
@@ -5,19 +5,31 @@ describe('dataFunctions', () => {
     const characterList = [
       {
         name: 'Arwen Evenstar',
+        category: 'elf',
+        description: 'Arwens description',
         significanceIndex: 14,
+        avatar: 'arwen_evenstar.jpg',
       },
       {
         name: 'Saruman the White',
+        category: 'wizard',
+        description: 'Sarumans description',
         significanceIndex: 16,
+        avatar: 'saruman_the_white.jpg',
       },
       {
         name: 'Galadriel',
+        category: 'elf',
+        description: 'Galadriels description',
         significanceIndex: 15,
+        avatar: 'galadriel.jpg',
       },
       {
         name: 'Éomer',
+        category: 'human',
+        description: 'Eomers description',
         significanceIndex: 17,
+        avatar: 'eomer.jpg',
       },
     ];
 
@@ -67,6 +79,61 @@ describe('dataFunctions', () => {
       expect(result[1].name).toBe('Galadriel');
       expect(result[2].name).toBe('Saruman the White');
       expect(result[3].name).toBe('Éomer');
+    });
+  });
+
+  describe('filterByCategory', () => {
+    const characterList = [
+      {
+        name: 'Arwen Evenstar',
+        category: 'elf',
+        description: 'Arwens description',
+        significanceIndex: 14,
+        avatar: 'arwen_evenstar.jpg',
+      },
+      {
+        name: 'Saruman the White',
+        category: 'wizard',
+        description: 'Sarumans description',
+        significanceIndex: 16,
+        avatar: 'saruman_the_white.jpg',
+      },
+      {
+        name: 'Galadriel',
+        category: 'elf',
+        description: 'Galadriels description',
+        significanceIndex: 15,
+        avatar: 'galadriel.jpg',
+      },
+      {
+        name: 'Éomer',
+        category: 'human',
+        description: 'Eomers description',
+        significanceIndex: 17,
+        avatar: 'eomer.jpg',
+      },
+    ];
+
+    it('returns only and all the characters of the category argument passed', () => {
+      expect(filterByCategory(characterList, 'human').length).toBe(1);
+      expect(filterByCategory(characterList, 'human')[0].category).toBe(
+        'human'
+      );
+      expect(filterByCategory(characterList, 'human')[0].name).toBe('Éomer');
+
+      expect(filterByCategory(characterList, 'elf').length).toBe(2);
+      expect(filterByCategory(characterList, 'elf')[0].category).toBe('elf');
+      expect(filterByCategory(characterList, 'elf')[1].category).toBe('elf');
+      expect(filterByCategory(characterList, 'elf')[0].name).toBe(
+        'Arwen Evenstar'
+      );
+      expect(filterByCategory(characterList, 'elf')[1].name).toBe('Galadriel');
+    });
+
+    it('returns an empty array if no characters match the category argument passed', () => {
+      expect(filterByCategory(characterList, 'corrupted hobbit')).toStrictEqual(
+        []
+      );
     });
   });
 });
