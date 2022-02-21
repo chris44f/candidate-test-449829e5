7c5ca4d2089c56a5321c3f049038dfa09d374645
chrisfeatley <chris.featley@yodel.co.uk>
2022-02-21 06:44:11 +0000

    Updated index page test

--- src/components/IndexPage/IndexPage.test.js
+++ src/components/IndexPage/IndexPage.test.js
@@ -1,9 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import '@testing-library/jest-dom';
 import IndexPage from './IndexPage';
+import { render, screen, waitFor } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<IndexPage />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('IndexPage', () => {
+  beforeEach(() => {
+    render(<IndexPage />);
+  });
+
+  it('renders title', () => {
+    const { getByRole } = screen;
+    expect(
+      getByRole('heading', { name: /Lord of the Rings Character Index/i })
+    ).toBeInTheDocument();
+  });
+
+  it('renders logo image', () => {
+    const { getByRole } = screen;
+
+    expect(getByRole('img', { name: /logo/i })).toBeInTheDocument();
+  });
+
+  it('renders loading text', () => {
+    const { getByText } = screen;
+
+    expect(getByText('Loading...')).toBeInTheDocument();
+  });
+
+  describe('when api resolves', () => {
+    it('stops loading and renders filter and sort options', async () => {
+      await waitFor(() => {
+        expect(screen.queryByText('Loading...')).toBeFalsy();
+        expect(
+          screen.getByRole('combobox', { name: /Category/i })
+        ).toBeInTheDocument();
+        expect(
+          screen.getByRole('combobox', { name: /Order by/i })
+        ).toBeInTheDocument();
+      });
+    });
+  });
 });
